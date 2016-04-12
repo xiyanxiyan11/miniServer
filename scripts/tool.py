e29833dab1b9ba96c119b5e670e1767eb8afb46a
xiyanxiyan10 <xiyanxiyan10@hotmail.com>
2016-04-13 00:01:38 +0800

    init tool for match file

--- scripts/tool.py
+++ scripts/tool.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+
+import os, sys
+import re
+import filecmp
+
+class MisakaTool(object):
+    """manager tool for debug"""
+    def matchfile(self, reg, dstfile):
+        p = re.compile(reg)
+        for mfile in os.listdir('./'):
+                if mfile == dstfile:
+                    continue
+                if NONE == p.match(mfile):
+                    continue
+                if not filecmp.cmp(mfile, dstfile):
+                    print("file %s missmatch " % mfile)
