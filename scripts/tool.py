c80ab8774262e31cd4fbbc140f59da591be207eb
xiyanxiyan10 <xiyanxiyan10@hotmail.com>
2016-04-14 19:22:48 +0800

    match function add into tool

--- scripts/tool.py
+++ scripts/tool.py
@@ -2,17 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import sys, getopt
 import re
 import filecmp
+import sys
 
 class MisakaTool(object):
     """manager tool for debug"""
     def matchfile(self, reg, dstfile):
         p = re.compile(reg)
         for mfile in os.listdir('./'):
-                if mfile == dstfile:
+            if mfile == dstfile:
                     continue
-                if NONE == p.match(mfile):
+            if None == p.match(mfile):
                     continue
-                if not filecmp.cmp(mfile, dstfile):
-                    print("file %s missmatch " % mfile)
+            if not filecmp.cmp(mfile, dstfile):
+                    print("%s->%s missmatch " % (mfile, dstfile) )
+            else:
+                    pass
+                    #print("%s->%s match " % (mfile, dstfile) )
+
+    def help(self):
+        print("use [regex] [dstfile]")
+
+if __name__ == '__main__':
+    mtool = MisakaTool()
+    srcfile = None
+    dstfile = None
+    opts, args = getopt.getopt(sys.argv[1:], "h:s:d:")
+    for op, val in opts:
+        if op == "-s":
+            srcfile = val
+        elif op == "-d":
+            dstfile = val
+        else:
+            MisakaTool.help()
+
+    mtool.matchfile(srcfile, dstfile)
+
+
+
+
+
+
