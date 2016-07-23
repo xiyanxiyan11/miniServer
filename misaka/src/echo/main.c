b481274011b764b9757929da28f4394f2c344612
面码酱 <xiyanxiyan10@gmail.com>
2016-07-23 17:28:50 +0800

    delete api not used

--- misaka/src/echo/main.c
+++ misaka/src/echo/main.c
@@ -14,23 +14,25 @@ int core_register()
 
     SET_DEBUG(DEBUG_DEBUG);
     misaka_config.role = ROLE_SERVER;
+
+
+//support support test
 #if 0
-    //register udp 
     peer  =  udp_init("192.168.97.98", 8000, 11112);
     if(!peer){
         fprintf(stderr, "init peer fail\n");
 	    return -1;
     }
-
     peer->pack =    echo_pack;
     peer->unpack =  echo_unpack;
     peer->role = ROLE_SERVER;
     peer->drole = ROLE_UDP;
     peer_register(peer);
     misaka_start(peer);
-
 #endif
 
+
+//tcp support test
 #if 1
     peer = tcp_listen_init(TCP_DSTPORT);
     peer->listens = 3;
@@ -42,15 +44,6 @@ int core_register()
     misaka_start(peer);
 
 #endif
-
-#if 0
-    peer = tcp_connect_init("127.0.0.1", TCP_DSTPORT, 11000);
-    peer->role = ROLE_TCP;
-    peer->drole = ROLE_SERVER;
-    peer_register(peer);
-    misaka_start(peer);
-#endif
-
     return 0;
 }
 
