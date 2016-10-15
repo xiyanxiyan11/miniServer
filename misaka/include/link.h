b11d3067aa50dfacdeca237f49a3c21ca974c871
面码酱 <xiyanxiyan10@gmail.com>
2016-10-15 17:55:35 +0800

    Update link.h

--- misaka/include/link.h
+++ misaka/include/link.h
@@ -6,4 +6,5 @@ struct peer * ludp_init(const char *srcip, const char *dstip);
 struct peer * sdio_init(const char *str);
 struct peer * tcp_listen_init(int srcport);
 struct peer * tcp_connect_init(const char *str, unsigned short dstport, unsigned short srcport);
+
 #endif
