f9dc434cf1b435dd824b12fc21245c55d578462d
面码酱 <xiyanxiyan10@gmail.com>
2016-11-01 09:52:33 +0800

    rebuild parser trigger

--- misaka/parser/normal_parser.c
+++ misaka/parser/normal_parser.c
@@ -2,13 +2,13 @@
 
 /*pay load as this 
 |------------------------------------------------
-|magic|src|dst|type|nid|sid|seq|data len|payload|
+|magic   |nid|dst|type|sid|seq|payloadLen|payloadData|
 |------------------------------------------------
-* 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 = 32
+* 8 +      4 + 4 + 4 +  4 + 4 +   4       =   32
 */
 
-#define MAGIC_STR "mv01"
-#define MAGIC_STR_SIZE  (4)
+#define MAGIC_STR "misaka01"
+#define MAGIC_STR_SIZE  (8)
 #define MAGIC_HEADER_SIZE (32) 
 
 int normal_parser_init(struct peer *peer){
@@ -28,16 +28,13 @@ int normal_parser_unpack(struct stream *s, struct peer *peer){
             return IO_PARTIAL; 
         }else{
             if(0 != memcmp(STREAM_PNT(s), MAGIC_STR, MAGIC_STR_SIZE)){
-                stream_reset(s);
-                peer->packet_size = MAGIC_HEADER_SIZE;
-                return IO_PARTIAL;
+                return IO_CLOSE;
             }
         }
         s->getp += MAGIC_STR_SIZE;
-        s->src = stream_getl(s);
+        s->nid = stream_getl(s);
         s->dst = stream_getl(s);
         s->type = stream_getl(s);
-        s->nid = stream_getl(s);
         s->sid = stream_getl(s);
         s->seq = stream_getl(s);
         length = stream_getl(s);
@@ -45,7 +42,6 @@ int normal_parser_unpack(struct stream *s, struct peer *peer){
         peer->packet_size = length;
         return IO_PARTIAL;
     }
-    //read all body size
     return IO_PACKET;
 }
 
