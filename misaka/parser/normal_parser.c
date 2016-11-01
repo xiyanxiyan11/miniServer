aca0f99abe3b52ae2294bfaa2625a8c51b1f9c20
面码酱 <xiyanxiyan10@gmail.com>
2016-11-01 20:06:55 +0800

    1.parser pack trigger

--- misaka/parser/normal_parser.c
+++ misaka/parser/normal_parser.c
@@ -2,15 +2,16 @@
 
 /*payload format
 |-----------------------------------------------------
-|magic   |nid|dst|type|sid|seq|payloadLen|payloadData|
+|magic   |nid|src|dst|type|sid|seq|payloadLen|payloadData|
 |-----------------------------------------------------
-* 8 +      4 + 4 + 4 +  4 + 4 +   4       =   32
+* 8 +      4 + 4 + 4 + 4 +  4 + 4 +   4       =   32
 */
 
-#define MAGIC_STR "misaka01"
-#define MAGIC_STR_SIZE  (8)
+#define MAGIC_STR "miku1"
+#define MAGIC_STR_SIZE  (4)
 #define MAGIC_HEADER_SIZE (32) 
 
+//init parser stream
 int normal_parser_init(struct peer *peer){
     peer->header_size = MAGIC_HEADER_SIZE;
     peer->body_size = 0;
@@ -32,6 +33,7 @@ int normal_parser_unpack(struct stream *s, struct peer *peer){
         }
         s->getp += MAGIC_STR_SIZE;
         s->nid = stream_getl(s);
+        s->src = stream_getl(s);
         s->dst = stream_getl(s);
         s->type = stream_getl(s);
         s->sid = stream_getl(s);
@@ -44,7 +46,24 @@ int normal_parser_unpack(struct stream *s, struct peer *peer){
     return IO_PACKET;
 }
 
-//@TODO no pack, write header when writer trigger
-int  normal_parser_pack(struct stream *s, struct peer *peer){
+//reverse header ofr pack
+int normal_parser_reverse(struct stream *s){
+    s->getp +=  MAGIC_HEADER_SIZE;
+    return 0;
+}
+
+//init thread header for the packet wait to send
+int normal_parser_pack(struct stream *s, struct peer *peer){
+    int body_size = stream_get_endp(s) - stream_get_getp(s);
+    s->getp = 0;
+    stream_put (s, MAGIC_STR, MAGIC_STR_SIZE);
+    stream_putl(s, s->nid);
+    stream_putl(s, s->src);
+    stream_putl(s, s->dst);
+    stream_putl(s, s->type);
+    stream_putl(s, s->sid);
+    stream_putl(s, s->seq);
+    stream_putl(s, body_size);
+    s->getp = 0;
     return IO_PACKET;
 }
